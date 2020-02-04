3be6d84d1dd4d85f4ae300ed207d346cf0eb4874
Sop Vong <sopvong@github.com>
2020-02-04 18:52:51 -0500

    After seeing Christian on 02/04

--- src/sudba.c
+++ src/sudba.c
@@ -7,10 +7,12 @@
 
 int main(int argc, char *argv[])
 {
-	openlog("sudba", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1); //Why in the assignment you said "sudba" less the quotes?
+	openlog("sudba", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1); //RESOLVED! Why in the assignment you said "sudba" less the quotes????????????????????????????????
 	char *r = sudba_read_line(0);
 	printf("r[0] is %c", r[0]); //testing purposes
+	if (r != NULL) free(r); //Free only if the pointer is not null. 
 	closelog();
+	
 
   return EXIT_SUCCESS;
 }
--- src/sudba_interface.c
+++ src/sudba_interface.c
@@ -12,11 +12,19 @@ char *sudba_read_line(int file)
 	int count = read(file, buf, SUDBA_MAX_INPUT); //Reads and stores the number of characters in buf, up to SUDBA_MAX_INPUT
 	printf("Count: %d\n", count); // testing purposes
 	char *r = malloc(count + 1); //Allocates dynamic memory r with size of the input +1 for the NULL-terminated character
- 
+	//strerror_r(file, buf, SUDBA_MAX_INPUT); //file might not be the correct argument !!
+ 	int i = 0;
+	while (i < count) { //This while loop copies the iTh character from the static buffer buf[SUDBA_MAX_INPUT] and stores it in the dynamic buffer r
+		r[i] = buf[i];
+		i = i + 1;
+	}
+	r[i] = 0; //RESOLVED! Ask Dmitry is index i+1 supposed to be there????????????????????????????????
 
 
-	//int done = read(file, buf, SUDBA_MAX_INPUT);
-	//printf("done = %d", done); //Ask Dmitry how to use read in a while loop, so that it isn't asking for another input, basically how to read again and pick up from where it left the previous one. !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	/*int done = 0;
+	while (done == 0) {
+		if (read(file, buf, SUDBA_MAX_INPUT) == 0) done = 1;
+	} */ 
 	//done = read(file, buf, SUDBA_MAX_INPUT);
 	//printf("new done = %d", done);
 	/*while (done != 0) {
@@ -28,21 +36,18 @@ char *sudba_read_line(int file)
 
 
 
-	int i = 0;
-	while (i < count) { //This while loop copies the iTh character from the static buffer buf[SUDBA_MAX_INPUT] and stores it in the dynamic buffer r
-		r[i] = buf[i];
-		i = i + 1;
-	}
-	printf("Final int i is %d\n", i); //testing purposes
-	r[i-1] = '0'; //Ask Dmitry is index i+1 supposed to be there? !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-	int len = strlen(r); //this is for testing purposes to print each index
-	for (int c = 0; c <= len; c = c+1){ //this is for testing purposes to print each index
+	//printf("Final int i is %d\n", i); //testing purposes
+	
+
+	//int len = strlen(r); //this is for testing purposes to print each index
+	
+	puts(r); //testing purposes
+	/*for (int c = 0; c <= len; c = c+1){ //this is for testing purposes to print each index
 	printf("r[%d] : %c \n",c,r[c]); //this is for testing purposes to print each index
-	}
+	}*/
 
 
-	//free(*buf); // Ask Dmitry, we can't free buf, but when we free r then it don't work in sudba.c !!!!!!!!!!!!!!!!
 	
 
   return r; // Must be changed
