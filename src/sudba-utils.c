b0b401328062147eca95a5ad816a687b19727711
nijacool <csalas@englishhs.org>
2020-02-27 08:32:59 -0500

    New changes, this is the right code, Git was not collecting the proper changes when I was pushing

--- src/sudba-utils.c
+++ src/sudba-utils.c
@@ -8,46 +8,37 @@
 
 char *sudba_make_string(char *text)
 {	
+	
 	int text_length = strlen(text); // Do i need to catch this?
   	char *temp = malloc(text_length+1);
 	int temp_counter = 0;
 	for (int i = 1; i < text_length-1; i++) {
 		if (text[i] == '\\') {
-//New Changes made from this point
 			switch(text[i+1]){
-				case '"': temp[temp_counter] = text[i+1];
-					   break;
-				case 't': temp[temp_counter] = '\t';
-					   break;
-				case 'r': temp[temp_counter] = '\r';
-					   break;
-				case '\\':temp[temp_counter] = '\\';
-					   break;
-			}
-		
-		}else{temp[temp_counter] = text[i];temp_counter++;}
-
-//Code Taken Care as a switch statement, but kept the if statement version until clean up stage of the homework, to compare and check for errors
-
-/*		
-			if(text[i+1] == '"') {temp[temp_counter] = text[i+1];}
-			if(text[i+1] == 't') {temp[temp_counter] = '\t';}
-			if(text[i+1] == 'n') {temp[temp_counter] = '\n';}
-			if(text[i+1] == 'r') {temp[temp_counter] = '\r'; //ask dz about r
-		}
-			if(text[i+1] == '\\') {temp[temp_counter] = '\\';
-		} //remaining backslash??
+				case '"': temp[temp_counter] = text[i+1];break;
+			//if(text[i+1] == '"') {temp[temp_counter] = text[i+1];}
+				case 't': temp[temp_counter] = '\t';break;
+			//if(text[i+1] == 't') {temp[temp_counter] = '\t';}
+				case 'n': temp[temp_counter] = '\n';break;
+			//if(text[i+1] == 'n') {temp[temp_counter] = '\n';}
+				case 'r': temp[temp_counter] = '\r';break;
+			//if(text[i+1] == 'r') {temp[temp_counter] = '\r'; //ask dz about r
+			//}
+				case '\\': temp[temp_counter] = '\\';break;
+			//if(text[i+1] == '\\') {temp[temp_counter] = '\\';} //remaining backslash??
 			//text_length--; //for realloc
+			}	
+			temp_counter++;
+			i++;
 		}
-		else {temp[temp_counter] = text[i];temp_counter++;}
-*/
-
-
+		else {
+			temp[temp_counter] = text[i];
+			temp_counter++;
+		}
 		printf("text[%i] = %c\n", i,text[i]);
 	}
-
-	temp[temp_counter] = '\0';
-	temp = realloc(temp,temp_counter);
+	temp[text_length] = '\0';
+	//temp = realloc(temp,text_length);
 	puts(temp);
 	//for (int j = 0; j < text_length; j++){
 	//	printf("temp[%i] = %c\n", j,temp[j]);
